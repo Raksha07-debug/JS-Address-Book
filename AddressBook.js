24b04055c64bd34294a0ac57862542d67613e6bf
Raksha <rak070803@gmail.com>
2025-03-04 13:05:21 +0530

    [Raksha] add. ABility to find existing conatct using their name and edit it

--- AddressBook.js
+++ AddressBook.js
@@ -45,6 +45,34 @@ class AddressBook{
             throw new error("Invalid Contact !")
         }
     }
+    editContact(firstName, updatedDetails) {
+        let contact = this.contacts.find(c => c.firstName === firstName);
+        if (!contact) {
+            console.log("Contact not found!");
+            return;
+        }
+
+        try {
+            let updatedContact = new Contact(
+                updatedDetails.firstName || contact.firstName,
+                updatedDetails.lastName || contact.lastName,
+                updatedDetails.address || contact.address,
+                updatedDetails.city || contact.city,
+                updatedDetails.state || contact.state,
+                updatedDetails.zip || contact.zip,
+                updatedDetails.phone || contact.phone,
+                updatedDetails.email || contact.email
+            );
+
+            let index = this.contacts.indexOf(contact);
+            this.contacts[index] = updatedContact;
+            console.log("Contact updated successfully!");
+
+        } catch (error) {
+            console.error("Update failed:", error.message);
+        }
+    }
+
     displayContacts(){
         console.log("Address Book",this.contacts);
     }
@@ -57,6 +85,9 @@ try{
     addressBook.addContact(contact1);
     addressBook.addContact(contact2);
     addressBook.displayContacts();
+
+    addressBook.displayContacts();
+    addressBook.editContact("Raksha", { city: "Amsterdam" });
 }catch(error){
     console.error(error.message);
 }
